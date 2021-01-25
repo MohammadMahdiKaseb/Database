735a259528e6f26278c675d374086f80e0c639ab
poulstar <poulstar@gmail.com>
2021-01-25 17:27:00 +0330

    test

--- DataBase/models.py
+++ DataBase/models.py
@@ -1,7 +1,11 @@
-
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column , String , INTEGER , DateTime, engine
-from sqlalchemy.sql.sqltypes import Integer
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy import (Column ,
+ String ,
+  Integer ,
+   Date ,
+    TIMESTAMP,
+     engine,
+     func)
 from connection import Connection
 
 
@@ -10,19 +14,22 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = "student"
-    studenId = Column(INTEGER() , primary_key=True , autoincrement=True)
-    name = Column(String(50))
-    family = Column(String(50))
+    studenId = Column(Integer() , primary_key=True , autoincrement=True)
+    frist_name = Column(String(50))
+    last_name = Column(String(50))
+    birth_data = Column(Date())
     grade = Column(Integer)
-    created_at = Column(DateTime())
+    level = Column()
+    update_at = Column(TIMESTAMP ,
+     server_default=func.now()
+     ,onupdate=func.cucurrent_timestamp())
 
 
-
-    def __init__(self , name = None , family = None , grade = None ):
+    def __init__(self , name = None , family = None , grade = None , level = None):
      self.name = name
      self.family = family 
      self.grade = grade
-
+     self.level = level
 
 
 engine = Connection().get_connection()
